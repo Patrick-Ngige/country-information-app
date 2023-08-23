7406de268ce2298bc8e26814c7eec8a346ccb494
Patrick-Ngige <wakemanjajr@gmail.com>
2023-08-23 21:28:52 +0300

    adding buttons infront os search results

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
-  const [results, setResults] = useState(null);
+  const [results, setResults] = useState([]);
 
   const handleSearch = async () => {
     if (!searchQuery) return;
