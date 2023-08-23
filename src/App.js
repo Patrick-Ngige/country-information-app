37b078ae5f36bf1522e26176f3bac24a6ecc8a25
Patrick-Ngige <wakemanjajr@gmail.com>
2023-08-23 17:17:26 +0300

    currency exchange app

--- src/App.js
+++ src/App.js
@@ -11,9 +11,14 @@ function App() {
     const response = await fetch(`https://restcountries.com/v3.1/name/${searchQuery}`)
     const data = await response.json()
 
-    if(data.length > 0) {
+    if(data.length > 10) {
       setResults(
         <p>Too many matches. Please make your query more specific.</p>
+      )
+    } else if (data.length > 1)  {
+      const countriesList = data.map(country => country.name.common).join(',')
+      setResults(
+        <p>Matching countries: {countriesList}</p>
       )
     }
   }
